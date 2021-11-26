70b5df4662cfe70eb3a66087a8b477c873d5b1b5
Akande Al-ameen <longlasting1805@gmail.com>
2021-11-26 18:48:20 +0000

    wsgi fix

--- Exchange_rate/wsgi.py
+++ Exchange_rate/wsgi.py
@@ -13,6 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Exchange_rate.settings')
 
+
 application = get_wsgi_application()
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+
+
